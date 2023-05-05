e46822bbf3d53f555708a9c834d025d4e4a581b4
Saysuu <skysaika@yandex.ru>
2023-05-06 00:43:39 +0300

    добавила sql-команды создания таблиц из pgadmin

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,27 @@
 -- SQL-команды для создания таблиц
+CREATE TABLE customers
+(
+	customer_id varchar(10) PRIMARY KEY,
+	company_name varchar(50) NOT NULL,
+	contact_name varchar(50) NOT NULL
+);
+
+CREATE TABLE employees
+(
+	employee_id serial PRIMARY KEY,
+	first_name varchar(20) NOT NULL,
+	last_name varchar(20) NOT NULL,
+	title text NOT NULL,
+	birth_date varchar(10) NOT NULL,
+	notes text NOT NULL
+);
+
+CREATE TABLE orders
+(
+	order_id integer NOT NULL,
+	customer_id varchar(10) REFERENCES customers(customer_id) NOT NULL,
+	employee_id integer REFERENCES employees(employee_id) NOT NULL,
+	order_date varchar(10) NOT NULL,
+	ship_city varchar(30) NOT NULL
+
+);
