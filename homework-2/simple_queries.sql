29d80fcbd0572bdad7a2c24a80015a57edc6f93e
Saysuu <skysaika@yandex.ru>
2023-05-07 13:49:20 +0300

    внесла 5 операций в simle_queries.sql

--- homework-2/simple_queries.sql
+++ homework-2/simple_queries.sql
@@ -4,6 +4,7 @@ SELECT contact_name, city
 FROM customers;
 
 -- 2. идентификатор заказа и разницу между датами формирования (order_date) заказа и его отгрузкой (shipped_date) из таблицы orders
+
 SELECT order_id, shipped_date - order_date
 FROM orders;
 
@@ -18,7 +19,6 @@ FROM orders;
 -- 5. количество стран, в которые отгружался товар (таблица orders, колонка ship_country)
 SELECT COUNT(DISTINCT ship_country)
 FROM orders
-
 
 
 
